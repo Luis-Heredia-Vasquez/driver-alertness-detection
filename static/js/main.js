94f8d236c2b7e5e778d99cb1381d1281e6dca461
Luis-Heredia-Vasquez <lheredia.projects@gmail.com>
2026-06-05 20:03:56 +1000

    fix: webcam feed and chart rendering

--- static/js/main.js
+++
@@ -1,3 +0,0 @@
-// This file is deprecated; use dashboard.js instead
-console.log('Dashboard initialized');
-
--- tests/test_utils.py
+++
@@ -1,6 +0,0 @@
-from src.utils.config import load_config
-
-
-def test_load_config():
-    cfg = load_config()
-    assert 'default' in cfg
